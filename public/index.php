4ba1e3ad51c191272c90281bdbe71c64d2e3e8d6
alican uzundemir <alican.uzundemir@gmail.com>
2021-10-26 22:44:52 +0300

    proje main kısmı eklendi(proje added part)

--- public/index.php
+++ public/index.php
@@ -1,55 +1,334 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-define('LARAVEL_START', microtime(true));
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <title>RESTAURANT</title>
+</head>
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+<body>
+    <div id="header">
+        <div class="headerBg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4 headerImg">
+                        <img src="images/LOGO.png" alt="logo">
+                    </div>
+                    <div class="col-8 d-flex justify-content-end">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <div class="container-fluid">
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
-}
+                                <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item">
+                                            <a class="nav-link" aria-current="page" href=""><b>HOME</b></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" aria-current="page" href=""><b>ABOUT</b></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href=""><b>LOCATIONS</b></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href=""><b>MENU</b></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link "><b>CONTACT</b></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div id="header-content">
+            <div class="container">
+                <div id="slider-1" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#slider-1" data-slide-to="0" class="active"></li>
+                        <li data-target="#slider-1" data-slide-to="1"></li>
+                        <li data-target="#slider-1" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="row">
+                                <div class="col-1"></div>
+                                <div class="col-7" style="margin-left: 35px;">
+                                    <h4>What is Pasta?</h4>
+                                    <p class="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque autem blanditiis corporis culpa debitis
+                                        dicta distinctio dolor dolore earum, eius eveniet explicabo fugit harum illo iste iure iusto modi nostrum odio optio pariatur
+                                        perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem. Ducimus, repellendus?
+                                        nostrum odio optio pariatur perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem.
+                                        Ducimus, repellendus?</p>
+                                </div>
+                                <div class="col-3">
+                                    <img src="images/slideimg.png" alt="" class="d-block w-100">
+                                </div>
+                                <div class="col-1"></div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-1"></div>
+                                <div class="col-7" style="margin-left: 35px;">
+                                    <h4>What is Pasta?</h4>
+                                    <p class="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque autem blanditiis corporis culpa debitis
+                                        dicta distinctio dolor dolore earum, eius eveniet explicabo fugit harum illo iste iure iusto modi nostrum odio optio pariatur
+                                        perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem. Ducimus, repellendus?
+                                        nostrum odio optio pariatur perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem.
+                                        Ducimus, repellendus?</p>
+                                </div>
+                                <div class="col-3">
+                                    <img src="images/slideimg.png" alt="" class="d-block w-100">
+                                </div>
+                                <div class="col-1"></div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-1"></div>
+                                <div class="col-7" style="margin-left: 35px;">
+                                    <h4>What is Pasta?</h4>
+                                    <p class="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque autem blanditiis corporis culpa debitis
+                                        dicta distinctio dolor dolore earum, eius eveniet explicabo fugit harum illo iste iure iusto modi nostrum odio optio pariatur
+                                        perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem. Ducimus, repellendus?
+                                        nostrum odio optio pariatur perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem.
+                                        Ducimus, repellendus?</p>
+                                </div>
+                                <div class="col-3">
+                                    <img src="images/slideimg.png" alt="" class="d-block w-100">
+                                </div>
+                                <div class="col-1"></div>
+                            </div>
+                        </div>
+                        <a href="#slider-1" class="carousel-control-prev" data-slide="prev">
+                            <span class="arrow"><i class="fas fa-chevron-left fa-2x"></i></span>
+                        </a>
+                        <a href="#slider-1" class="carousel-control-next" data-slide="next">
+                            <span class="arrow"><i class="fas fa-chevron-right fa-2x"></i></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+    <main>
+        <div id="main-content">
 
-require __DIR__.'/../vendor/autoload.php';
+            <div class="container">
+                <div id="slider-2" class="carousel slide" data-ride="carousel">
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+                    <div class="carousel-inner">
+                        <div id="dishes">
+                            <img src="images/rectangle.png" alt="">
+                            <h3>BEST DISHES</h3>
+                            <img src="images/rectangle.png" alt="">
+                        </div>
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+                        <div class="carousel-item active">
 
-$kernel = $app->make(Kernel::class);
+                            <div class="row ordermakarone">
+                                <div class="col-3 makarone">
 
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
+                                    <img src="images/thumb1.png" alt="" class="d-block w-100">
+                                    <h4>PASTA CAPELLINI</h4>
+                                </div>
+                                <div class="col-3 makarone">
 
-$kernel->terminate($request, $response);
+                                    <img src="images/thumb2.jpg" alt="" class="d-block w-100">
+                                    <h4>PASTA PILLUS</h4>
+                                </div>
+                                <div class="col-3 makarone">
+
+                                    <img src="images/thumb3.png" alt="" class="d-block w-100">
+                                    <h4>PASTA FUSILLI</h4>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row ordermakarone">
+
+                                <div class="col-3 makarone" style="margin-left: 35px;">
+
+                                    <img src="images/thumb1.png" alt="" class="d-block w-100">
+                                    <h4>PASTA CAPELLINI</h4>
+                                </div>
+                                <div class="col-3 makarone">
+
+                                    <img src="images/thumb2.jpg" alt="" class="d-block w-100">
+                                    <h4>PASTA PILLUS</h4>
+                                </div>
+                                <div class="col-3 makarone">
+
+                                    <img src="images/thumb3.png" alt="" class="d-block w-100">
+                                    <h4>PASTA FUSILLI</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row ordermakarone">
+
+                                <div class="col-3 makarone" style="margin-left: 35px;">
+
+                                    <img src="images/thumb1.png" alt="" class="d-block w-100">
+                                    <h4>PASTA CAPELLINI</h4>
+                                </div>
+                                <div class="col-3 makarone">
+
+                                    <img src="images/thumb2.jpg" alt="" class="d-block w-100">
+                                    <h4>PASTA PILLUS</h4>
+                                </div>
+                                <div class="col-3 makarone">
+
+                                    <img src="images/thumb3.png" alt="" class="d-block w-100">
+                                    <h4>PASTA FUSILLI</h4>
+                                </div>
+                            </div>
+                        </div>
+
+                        <a href="#slider-2" class="carousel-control-prev1" data-slide="prev">
+                            <span class="arrow"><i class="fas fa-chevron-left fa-2x"></i></span>
+                        </a>
+                        <a href="#slider-2" class="carousel-control-next1" data-slide="next">
+                            <span class="arrow"><i class="fas fa-chevron-right fa-2x"></i></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="book-online">
+            <div class="container">
+
+                <div id="book-online-id">
+                    <img src="images/rectangle.png" alt="">
+                    <h3>BOOK ONLINE</h3>
+                    <img src="images/rectangle.png" alt="">
+                </div>
+
+                <div class="row book-input">
+
+                    <div class="col-6  nameIcon">
+                        <input type="text" placeholder="Your Name">
+                        <i class="fas fa-id-card" aria-hidden="true"></i>
+
+                    </div>
+                    <div class="col-6  nameIcon">
+                        <input type="text" placeholder="Your E-MAIL">
+                        <i class="fas fa-envelope-open-text"></i>
+                    </div>
+
+                </div>
+
+                <div class="row book-input">
+
+                    <div class="col-4  nameIcon2">
+                        <input type="text" placeholder="09-06-2014">
+                        <i class="fas fa-calendar-week"></i>
+
+                    </div>
+                    <div class="col-4  nameIcon2">
+                        <input type="text" placeholder="09-06-2014">
+                        <i class="fas fa-calendar-week"></i>
+                    </div>
+                    <div class="col-4  nameIcon2">
+                        <input type="text" placeholder="Number of persons">
+                        <i class="fas fa-male"></i>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+        </div>
+        <div class="book-now">
+            <div class="container">
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-8 bookNow">
+                        <h2>BOOK NOW</h2>
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+            </div>
+        </div>
+        <div id="contact-online">
+            <div class="container">
+
+                <div id="book-online-id">
+                    <img src="images/rectangle.png" alt="">
+                    <h3>CONTACT US</h3>
+                    <img src="images/rectangle.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="contact-us">
+            <div class="container">
+                <div class="row">
+                    <div class="dropdown">
+                        <img src="images/char3.png" alt="img3">
+                        <div class="dropdown_content">
+                            <img src="images/popup3.png" alt="img3">
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <img src="images/char4.png" alt="img2">
+                        <div class="dropdown_content">
+                            <img src="images/popup2.png" alt="img2">
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <img src="images/char5.png" alt="img5">
+                        <div class="dropdown_content">
+                            <img src="images/popup3.png" alt="img5">
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <img src="images/char6.png" alt="img6">
+                        <div class="dropdown_content">
+                            <img src="images/popup4.png" alt="img6">
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <img src="images/char1.png" alt="img1">
+                        <div class="dropdown_content">
+                            <img src="images/popup3.png" alt="img1">
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <img src="images/char2.png" alt="img2">
+                        <div class="dropdown_content">
+                            <img src="images/popup2.png" alt="img2">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
